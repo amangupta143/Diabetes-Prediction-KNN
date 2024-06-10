2e06b303ed6b8cdf70c9025358908a5b49e1591c
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-06-10 22:09:28 +0530

    Added web_app, model file and updated Diabetes-Prediction-Model.ipynb file
    Added Diabetes_Prediction_Web_App.py, trained_model.sav file and updated Diabetes-Prediction-Model.ipynb file

---
+++ Diabetes Prediction/Diabetes_Prediction_Web_App.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pickle
+import streamlit as st
+
+
+# loading the saved model
+# Replace file location at "open([FILE_LOCATION], 'rb')" with actual file location
+loaded_model = pickle.load(open("trained_model.sav", 'rb'))
+
+# creating a function for Prediction
+def diabetes_prediction(input_data):
+    # Convert input data to floats
+    input_data = [float(i) for i in input_data]
+    
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
+    else:
+      return 'The person is diabetic'
+  
+    
+def main():
+    
+    # giving a title
+    st.title('Diabetes Prediction Web App')
+    
+    
+    # getting the input data from the user
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure value')
+    SkinThickness = st.text_input('Skin Thickness value')
+    Insulin = st.text_input('Insulin Level')
+    BMI = st.text_input('BMI value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    Age = st.text_input('Age of the Person')
+    
+    # code for Prediction
+    diagnosis = ''
+    
+    # creating a button for Prediction
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+    st.success(diagnosis)
+    
+if __name__ == '__main__':
+    main()
